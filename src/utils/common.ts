0fddbc6debb56e79bb2ec273dcdacf4ad750719d
almeidx <almeidx@pm.me>
2022-07-10 20:44:59 +0100

    remove unused constants

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,16 +2,6 @@ import { env } from 'node:process';
 import type { Snowflake, User } from 'discord.js';
 import { DEFAULT_DELETE_MESSAGE_DAYS, ELLIPSIS_CHAR } from './constants.js';
 
-export const enum Time {
-  Seconds = 1000,
-  Minutes = Seconds * 60,
-  Hours = Minutes * 60,
-  Days = Hours * 24,
-  Weeks = Days * 7,
-  Years = Days * 365.24125,
-  Months = Years / 12,
-}
-
 export function makeUserInfo(user: User): string {
   return `${user.tag} (${user.id})`;
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,7 @@
 import { env } from 'node:process';
 import type { Snowflake } from 'discord.js';
-import { Time } from './common.js';
 
 export const GUILD_IDS: Snowflake[] = env.GUILD_IDS!.replaceAll(' ', '').split(',');
-
-export const RECENT_BAN_LIFETIME = 10 * Time.Minutes;
 
 export const ELLIPSIS_CHAR = '…';
 
