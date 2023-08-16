cb8db220c31890d16181e208ff38701902ea85cc
Almeida <almeidx@pm.me>
2023-08-16 21:26:49 +0100

    remove pino

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,17 +1,37 @@
-import pino from "pino";
-import type { PrettyOptions } from "pino-pretty";
+import { cyan, gray, red, yellow } from "colorette";
 
-export const logger = pino({
-	transport: {
-		target: "pino-pretty",
-		options: {
-			translateTime: "yyyy-mm-dd HH:MM:ss",
-			ignore: "pid,hostname",
-		} satisfies PrettyOptions,
-	},
-});
+const logPrefix = "LOG";
+const errorPrefix = red("ERROR");
+const infoPrefix = cyan("INFO");
+const warnPrefix = yellow("WARN");
 
-export const error = logger.error.bind(logger);
-export const fatal = logger.fatal.bind(logger);
-export const info = logger.info.bind(logger);
-export const warn = logger.warn.bind(logger);
+export function log(message: any, ...extra: any[]) {
+	console.log(prefix(message, logPrefix), ...extra);
+}
+
+export function error(message: any, ...extra: any[]) {
+	console.error(prefix(message, errorPrefix), ...extra);
+}
+
+export function info(message: any, ...extra: any[]) {
+	console.info(prefix(message, infoPrefix), ...extra);
+}
+
+export function warn(message: any, ...extra: any[]) {
+	console.warn(prefix(message, warnPrefix), ...extra);
+}
+
+function prefix(message: any, type: string) {
+	return `${gray(formatDate(new Date()))} [${type}] ${message}`;
+}
+
+function formatDate(date: Date) {
+	return (
+		`${date.getFullYear()}-${padNumber(date.getMonth() + 1)}-${padNumber(date.getDate())} ` +
+		`${padNumber(date.getHours())}:${padNumber(date.getMinutes())}:${padNumber(date.getSeconds())}`
+	);
+}
+
+function padNumber(num: number) {
+	return num.toString().padStart(2, "0");
+}
